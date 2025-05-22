68de01a976f96666a35b3f043a57dd072cfb88a3
ArginP <n7toman@gmail.com>
2025-05-22 22:04:25 +0300

    feat: add users route endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // GET /users
+export class UsersController {
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Put(':id') // PUT /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
